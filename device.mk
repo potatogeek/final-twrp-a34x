e71db7fcdda758a9e07f310cd71aae34f11b3021
potatogeek <gnogueira3164@gmail.com>
2024-12-11 11:23:11 -0300

    fix fastbootd and multidisabler

--- device.mk
+++ device.mk
@@ -1,8 +1,54 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 LOCAL_PATH := device/samsung/a34x
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 33
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := $(if $(filter 30,$(BOARD_SYSTEMSDK_VERSIONS)),30,32)
+
+# Soong
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl \
+    update_engine_client
+
+PRODUCT_PACKAGES += \
+    bootctrl.mt6877 \
+    bootctrl.mt6877.recovery
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
+    libhealthd.$(PRODUCT_PLATFORM)
+
+PRODUCT_PACKAGES += shrink
+
+# TWRP TZData
+PRODUCT_PACKAGES_ENG += \
+    tzdata_twrp
+
+# EROFS Tools
+PRODUCT_HOST_PACKAGES_ENG += \
+    liberofs \
+    mkfs.erofs \
+    make_erofs \
+    dump.erofs \
+    fsck.erofs
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
+    ro.product.system.model \
+    ro.product.vendor.model \
+    ro.product.odm.model \
+    ro.product.model \
+    ro.product.product.model \
+    ro.product.system_ext.model
--- twrp_a34x.mk
+++ twrp_a34x.mk
@@ -1,19 +1,19 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a34x device
 $(call inherit-product, device/samsung/a34x/device.mk)
+
 
 PRODUCT_DEVICE := a34x
 PRODUCT_NAME := twrp_a34x
